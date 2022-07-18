9163a5ae7d46b76bd4a8577fe66db58bb155f6f8
OliveiraDCI <oliveira.sein.dci@gmail.com>
2022-07-18 11:56:08 +0200

    Exercise concluded.

--- average.js
+++ average.js
@@ -5,7 +5,14 @@
 // 12 + 14 + 16 = 42
 // 42 / 3 = 14
 
-const average = () => {};
+const average = (...numbers) => {
+  let args = numbers;
+  let numsSum = 0;
+  for (let i = 0; i < args.length; i++) {
+    numsSum += args[i];
+  }
+  return numsSum / args.length;
+};
 
 console.log(average(0)); // -> 0
 console.log(average(1, 2)); // -> 1.5
--- sum.js
+++ sum.js
@@ -1,7 +1,14 @@
 // Create a function sum which accepts any amount of numbers and return the sum of their addition
 
-const sum = () => {};
+const sum = (...nums) => {
+  let args = nums;
+  let argsSum = 0;
+  for (let i = 0; i < args.length; i++) {
+    argsSum += args[i];
+  }
+  return argsSum;
+};
 
-console.log(sum(1)); // -> 1
+console.log(sum()); // -> 1
 console.log(sum(1, 15)); // -> 16
 console.log(sum(25, 25, 20)); // -> 70
